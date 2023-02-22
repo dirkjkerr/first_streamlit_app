0cd6a2a4a913bd8b5f396394ddb3807c90d4ff8c
dirkjkerr <126094520+dirkjkerr@users.noreply.github.com>
2023-02-22 16:20:34 +0000

    Update streamlit_app.py
    Added 2 additional text items

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinnach & Rocket Smoothie')
+streamlit.text('Hard-Boiled, Free-Range Egg')
